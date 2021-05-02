1dfb4fe5b2c86d944e541a6d3f65a31d4662dc5c
tojatos <tojatos@gmail.com>
2021-05-03 01:06:43 +0200

    standard brownian motion

--- Lectures.Tests/Lecture4Tests.fs
+++ Lectures.Tests/Lecture4Tests.fs
@@ -6,7 +6,13 @@ open FsUnit
 
 [<Test>]
 let ``normalDistRandom distribution test`` () =
-    let x = normalDistRandom 0. 0.5
-    Seq.take 10 x |> printfn "%A"
-    Seq.take 100 x |> printfn "%A"
+    let x = normalDistRandom 0. 0.5 1000
+    let wr = new System.IO.StreamWriter("E:\data\dist_test.csv")
+    Seq.take 1000 x |> Seq.map(string) |> String.concat(",") |> wr.Write
+    wr.Close()
+    true |> should equal true
+    
+[<Test>]
+let ``zad1 test`` () =
+    let result = zad1 1 100 0 0 0 5 1000
     true |> should equal true
--- Lectures/Lecture4.fs
+++ Lectures/Lecture4.fs
@@ -2,8 +2,9 @@
 
 open System
 
-let normalDistRandom mean stdDev = 
-    let rand = Random()
+// http://www.fssnip.net/o7/title/Simple-normally-distributed-random-number-generator
+let normalDistRandom mean stdDev seed = 
+    let rand = Random(seed)
     let rec polarBoxMullerDist () = seq {
             let rec getRands () =
                 let u = (2.0 * rand.NextDouble()) - 1.0
@@ -21,8 +22,37 @@ let normalDistRandom mean stdDev =
             yield mean + (x * stdDev); yield mean + (y * stdDev); yield! polarBoxMullerDist ()
         }
     polarBoxMullerDist ()
-
-//let zad1 count steps price drift vol years seed =
-//    let r = Random(seed)
-//    let exponent = (drift - pown vol 2) * 0
-//    let result = price * Math.Pow(Math.E, )
+    
+//let W t n randomVariables =
+//    let rec f last = seq {
+//        let next = last + sqrt (t/.n)
+//       
+//        
+//    }
+//    0 :: f 0
+//let W t last myNormalDistRandom =
+//    if t = 0 then 0𝜎
+//    else last +. (Seq.head myNormalDistRandom)
+//
+let zad1 N n S0 r vol t seed =
+    let div = ((float t) / (float n))
+    let x = sqrt(div)
+    let myNormalDistRandom = normalDistRandom 0. div seed
+    let randomVariables = Seq.take n myNormalDistRandom
+    let WSeq = (0., randomVariables) |> Seq.unfold(fun (a, b) ->
+            if (Seq.isEmpty b) then None
+            else
+                let next = a + (Seq.head b) * x
+                Some(a, (next, Seq.tail b))
+        )
+    WSeq |> Seq.length |> printfn "%A"
+    WSeq |> Seq.take n |> printfn "%A"
+    WSeq |> Seq.take n |> Seq.length |> printfn "%A"
+    
+//    let t = 0
+    
+//    let Wt = W t 0. myNormalDistRandom
+//    let exponent = (drift -. (pown vol 2)) * t + vol * Wt
+//    let result = price * Math.Pow(Math.E, exponent)
+    //    result
+    0
